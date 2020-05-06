595082287e036db390acbaa0c4e53eb4b55987f8
Tom Manner <tsmanner@us.ibm.com>
2020-05-06 16:59:41 -0400

    Added a little more testing to the SystemSnapshot relationship test.

--- tests/orm/relationship/test_SystemSnapshot.py
+++ tests/orm/relationship/test_SystemSnapshot.py
@@ -6,6 +6,8 @@ from taskobra.orm import get_engine, get_session, Snapshot, System
 class TestSystemSnapshot(ORMTestCase):
     def test_SystemSnapshot_creation(self):
         system = System()
-        system.snapshots.append(Snapshot(timestamp=datetime(2020, 3, 9, 9, 53, 53)))
+        snapshot = Snapshot(timestamp=datetime(2020, 3, 9, 9, 53, 53))
+        system.snapshots.append(snapshot)
 
         [self.assertIs(system, snapshot.system) for snapshot in system.snapshots]
+        self.assertIs(snapshot, system.snapshots[0])
