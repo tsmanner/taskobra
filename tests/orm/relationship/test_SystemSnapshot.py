7e6f5026fc9448cdd099c6f50541eb75c5781cfa
Tom Manner <tsmanner@us.ibm.com>
2020-05-06 17:04:04 -0400

    Added testing for bidirectional back populating in the SystemSnapshot relationship.

--- tests/orm/relationship/test_SystemSnapshot.py
+++ tests/orm/relationship/test_SystemSnapshot.py
@@ -5,9 +5,17 @@ from taskobra.orm import get_engine, get_session, Snapshot, System
 
 class TestSystemSnapshot(ORMTestCase):
     def test_SystemSnapshot_creation(self):
-        system = System()
-        snapshot = Snapshot(timestamp=datetime(2020, 3, 9, 9, 53, 53))
-        system.snapshots.append(snapshot)
+        with self.subTest("Snapshot added to System"):
+            system = System()
+            snapshot = Snapshot(timestamp=datetime(2020, 3, 9, 9, 53, 53))
+            system.snapshots.append(snapshot)
 
-        [self.assertIs(system, snapshot.system) for snapshot in system.snapshots]
-        self.assertIs(snapshot, system.snapshots[0])
+            [self.assertIs(system, snapshot.system) for snapshot in system.snapshots]
+            self.assertIs(snapshot, system.snapshots[0])
+        with self.subTest("System added to Snapshot"):
+            system = System()
+            snapshot = Snapshot(timestamp=datetime(2020, 3, 9, 9, 53, 53))
+            snapshot.system = system
+
+            [self.assertIs(system, snapshot.system) for snapshot in system.snapshots]
+            self.assertIs(snapshot, system.snapshots[0])
